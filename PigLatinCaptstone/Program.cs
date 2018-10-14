095cf1018ac0f85be2914fba5d7b1267e89b2b23
Sean Keckler <seankeckler@gmail.com>
2018-10-14 13:23:26 -0400

    added continue

--- PigLatinCaptstone/Program.cs
+++ PigLatinCaptstone/Program.cs
@@ -10,37 +10,47 @@ namespace PigLatinCaptstone
     {
         static void Main(string[] args)
         {
-            
-            Console.WriteLine("Pig Latin!");
-            Console.Write("Enter a word/sentence to be changed into pig latin: ");
-            string userEnteredWords = Console.ReadLine();
-            string [] split = userEnteredWords.Split();
+            bool run = true;
 
-            foreach (string beenSplit in split)
+            while (run == true)
             {
-                if (beenSplit.StartsWith("a") ||
-                    (beenSplit.StartsWith("e")) ||
-                    (beenSplit.StartsWith("i")) ||
-                    (beenSplit.StartsWith("o")) ||
-                    (beenSplit.StartsWith("u")))
+                Console.Clear();
+                Console.WriteLine("Pig Latin!");
+                Console.Write("Enter a word/sentence to be changed into pig latin: ");
+                string userEnteredWords = Console.ReadLine();
+                string[] split = userEnteredWords.Split();
+
+                foreach (string beenSplit in split)
                 {
-                    Console.WriteLine(beenSplit + "way");
+                    if (beenSplit.StartsWith("a") ||
+                        (beenSplit.StartsWith("e")) ||
+                        (beenSplit.StartsWith("i")) ||
+                        (beenSplit.StartsWith("o")) ||
+                        (beenSplit.StartsWith("u")))
+                    {
+                        Console.WriteLine(beenSplit + "way");
+                    }
+                    else if (!(beenSplit.StartsWith("a") ||
+                        (beenSplit.StartsWith("e")) ||
+                        (beenSplit.StartsWith("i")) ||
+                        (beenSplit.StartsWith("o")) ||
+                        (beenSplit.StartsWith("u"))))
+                    {
+                        string beenPigLatinified = PigLatinify(beenSplit);
+                        Console.WriteLine(beenPigLatinified);
+
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("I need you to enter a sentence or a word.");
+                    }
                 }
-                else if (!(beenSplit.StartsWith("a") ||
-                    (beenSplit.StartsWith("e")) ||
-                    (beenSplit.StartsWith("i")) ||
-                    (beenSplit.StartsWith("o")) ||
-                    (beenSplit.StartsWith("u"))))
-                {
-                    string beenLatinified = PigLatinify(beenSplit);
-                    Console.WriteLine(beenLatinified);
-                    
-                }
+                run = Continue();
+
             }
 
-
-
-            Console.ReadKey();
+            
 
         }
         public static string PigLatinify(string latined)
@@ -57,6 +67,30 @@ namespace PigLatinCaptstone
                 }
             }
             return latined;
+        }
+
+        public static bool Continue()
+        {
+            bool run;
+
+            Console.WriteLine("Do you want to do more translations? y/n");
+            string answer = Console.ReadLine();
+            answer = answer.ToLower();
+
+            if (answer == "y")
+            {
+                run = true;
+            }
+            else if (answer == "n")
+            {
+                run = false;
+            }
+            else
+            {
+                Console.WriteLine("Sorry that is not an acceptable answer");
+                run = Continue();
+            }
+            return run;
         }
 
 
